fc3688fe2aa48c80c0b7e95377b662318c73d650
Thanh <s3601172@student.rmit.edu.au>
2020-08-30 14:25:55 +1000

    switch view based on button press

--- FrontEnd/src/dashboard/Dashboard.tsx
+++ FrontEnd/src/dashboard/Dashboard.tsx
@@ -1,42 +1,97 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import User from '../interfaces/userinfo.interface';
 
-interface UserType{
-    userType: number
+interface DashboardStatus {
+  defaultView: boolean;
 }
 
-class Dashboard extends Component<{}, UserType> {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userType: 3
-        };
-    }
+class Dashboard extends Component<{ userId; userType }, User & DashboardStatus> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: props.userId,
+      userType: props.userType,
+      defaultView: true
+    };
+    this.upcomingView = this.upcomingView.bind(this);
+    this.completedView = this.completedView.bind(this);
+    this.getBookingRows = this.getBookingRows.bind(this);
+  }
 
-    render() {
-        if (this.state.userType == 3) {
-            return (
-                <div className="container">
-                    <table className="table table-hover">
-                        <th>
-                            <td><button type="button" className="btn btn-default"> Upcoming Bookings </button></td>
-                            <td><button type="button" className="btn btn-default"> Completed Bookings </button></td>
-                        </th>
-                        <tr>
-                            <td>Example Booking</td>
-                            <td><button type="button" className="btn btn-info">Manage Booking</button></td>
-                        </tr>
-                    </table>
-                </div>
-
-            );
-        }
-        else{
-            return(
-                <div className="center">
-                    error
-                </div>
-            )
-        }
+  upcomingView() {
+    if (this.state.defaultView == false) {
+      this.setState({ defaultView: true });
+      this.forceUpdate();
     }
+  }
+
+  completedView() {
+    if (this.state.defaultView == true) {
+      this.setState({ defaultView: false });
+      this.forceUpdate();
+    }
+  }
+
+  getBookingRows(id: string) {
+    if (this.state.defaultView == true) {
+      return (
+        <tbody>
+          <tr>
+            <td>Example Upcoming Booking</td>
+            <td>
+              <button type='button' className='btn btn-info'>
+                Manage Booking
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      );
+    } else if (this.state.defaultView == false) {
+      return (
+        <tbody>
+          <tr>
+            <td>Example Completed Booking</td>
+            <td>
+              <button type='button' className='btn btn-info'>
+                View Booking Details
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      );
+    } else {
+      return <div className='container'> An unexpected error has occured please refresh the page.</div>;
+    }
+  }
+
+  render() {
+    if (this.state.userType == 3) {
+      return (
+        <div className='container'>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>
+                  <button type='button' className='btn btn-primary active' onClick={this.upcomingView}>
+                    Upcoming Bookings
+                  </button>
+                </th>
+
+                <th>
+                  <button type='button' className='btn btn-primary' onClick={this.completedView}>
+                    Completed Bookings
+                  </button>
+                </th>
+              </tr>
+            </thead>
+
+            {this.getBookingRows(this.props.userId)}
+          </table>
+        </div>
+      );
+    } else {
+      return <div className='container'>An unexpeced error has occured please refresh the page.</div>;
+    }
+  }
 }
 export default Dashboard;
--- FrontEnd/src/index.tsx
+++ FrontEnd/src/index.tsx
@@ -4,4 +4,4 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Dashboard from "./dashboard/Dashboard";
 
-ReactDOM.render(<Dashboard />, document.getElementById('root'));
+ReactDOM.render(<Dashboard userId={"u1"} userType={3} />, document.getElementById('root'));
