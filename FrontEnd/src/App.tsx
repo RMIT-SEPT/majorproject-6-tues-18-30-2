561dba42802a1784d71be595d0e7d8017626b854
Nick Mladenov <s3539747@student.rmit.edu.au>
2020-09-02 04:21:53 +1000

    Implemented as basic (not necessarily needed) application (component) for rendering components and views within the frontend.

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/**
+ * App Properties
+ */
+export interface ComponentProps {}
+
+/**
+ * App Component
+ */
+const App: React.FC<ComponentProps> = () => {
+  return <h1>SEPT 2020 Frontend: Group 6.TUES-18.30-2</h1>;
+};
+
+/**
+ * Expose App
+ */
+export default App;
