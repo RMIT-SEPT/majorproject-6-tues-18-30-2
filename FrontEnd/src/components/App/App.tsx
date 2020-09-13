eb3395f71fad7c7f8f79f5279f2b8e3503b93fbd
Nick Mladenov <s3539747@student.rmit.edu.au>
2020-09-13 16:37:29 +1000

    Migrated the application as a component into the appropriate directory.

--- FrontEnd/src/components/App/App.tsx
+++ FrontEnd/src/components/App/App.tsx
@@ -8,11 +8,6 @@ export interface ComponentProps {}
 /**
  * App Component
  */
-const App: React.FC<ComponentProps> = () => {
+export const App: React.FC<ComponentProps> = () => {
   return <h1>SEPT 2020 Frontend: Group 6.TUES-18.30-2</h1>;
 };
-
-/**
- * Expose App
- */
-export default App;
