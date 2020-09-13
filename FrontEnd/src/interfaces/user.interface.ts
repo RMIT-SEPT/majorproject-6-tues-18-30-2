ba2ee483bf8cbf42ad2b53c583642b50939b9f98
Nick Mladenov <s3539747@student.rmit.edu.au>
2020-09-13 15:24:30 +1000

    Reformatted the user interface to apply the specified project configuration.

--- FrontEnd/src/interfaces/user.interface.ts
+++ FrontEnd/src/interfaces/user.interface.ts
@@ -1,13 +1,13 @@
 interface User{
-    userId:string,
-    userType: number,
-    userFirstName?: string,
-    userLastName?: string,
-    userEmail?: string,
-    userPhone?: number,
-    userAddressNo?: number,
-    userStreet?: string,
-    userState?: string
+  userId:string,
+  userType: number,
+  userFirstName?: string,
+  userLastName?: string,
+  userEmail?: string,
+  userPhone?: number,
+  userAddressNo?: number,
+  userStreet?: string,
+  userState?: string
 }
 
 export default User;
