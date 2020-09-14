57f6cba7edf8c0bb224f25422b0b8fe05f65872a
s3539747 <48439097+s3539747@users.noreply.github.com>
2020-09-14 16:54:33 +1000

    Frontend Routing (#9)
    * Added the reach router packages for configuring routing.
    
    * Migrated the application as a component into the appropriate directory.
    
    * Introduced component rollup for the application.
    
    * Added rollup to the application component functions and interfaces.
    
    * Updated the reference to the application component.
    
    * Implemented an initial home page for utilisation by routing.
    
    * Added the component rollup for the home page.
    
    * Implemented an initial dashboard page for utilisation by routing.
    
    * Added the component rollup for the dashboard page.
    
    * Implemented an overall page rollup so references aren't explicitly assigned to individual pages.
    
    * Implemented a route component for use by the reach router component for delegating paths to pages.
    
    * Added a component rollup for the route.
    
    * Implemented the route component rollup inside the generic components rollup.
    
    * Added a router inside of the application for navigation through to pages.
    
    * Simplified the dashboard component booking rows rendering through a ternary condition.
    
    * Added type definitions for reach router.
    
    * Simplified the route component with inference from reach routing types.
    
    * Applied linting to the solution after introducing routing amendments.

---
+++ FrontEnd/src/components/App/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Router } from '@reach/router';
+import { Route } from '../';
+import { Home, Dashboard } from '../../pages';
+
+/**
+ * App Properties
+ */
+export interface ComponentProps {}
+
+/**
+ * App Component
+ */
+export const App: React.FC<ComponentProps> = () => {
+  return (
+    <Router>
+      <Route page={<Home />} path='/' />
+      <Route page={<Dashboard />} path='/dashboard' />
+    </Router>
+  );
+};
---
+++ FrontEnd/src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+/**
+ * Home Page
+ */
+export const Home: React.FC = () => {
+  return <h1>Homepage</h1>;
+};
