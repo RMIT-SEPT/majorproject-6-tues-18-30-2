6553b21585a54fae42a915ec016b447331aa7047
Nick Mladenov <s3539747@student.rmit.edu.au>
2020-09-15 18:36:36 +1000

    Removed a full width enhancement with components that do not exist in this PR but will be added later.

--- FrontEnd/src/layouts/FullWidth/FullWidth.tsx
+++ FrontEnd/src/layouts/FullWidth/FullWidth.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Layout } from 'antd';
-import { Header, Footer } from '../../components';
 import * as styles from './FullWidth.style';
 
 /**
@@ -11,13 +10,11 @@ export const FullWidth: React.FC = ({ children }) => {
 
   return (
     <Layout className="layout" style={{ minHeight: '100vh' }}>
-      <Header />
       <Content>
         <div className={styles.contents}>
           { children }
         </div>
       </Content>
-      <Footer />
     </Layout>
   );
 };
