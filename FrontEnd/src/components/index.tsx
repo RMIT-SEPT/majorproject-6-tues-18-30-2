a6714ec280e2fd3ae46ac152f66c476143780e60
Nick Mladenov <s3539747@student.rmit.edu.au>
2020-09-16 20:36:32 +1000

    Added the menu item to the root component rollup.

--- FrontEnd/src/components/index.tsx
+++ FrontEnd/src/components/index.tsx
@@ -1,4 +1,5 @@
 export { App, AppProps } from './App';
+export { MenuItem, MenuItemProps } from './MenuItem';
 export { Booking, BookingProps } from './Booking';
 export { Dashboard, DashboardProps } from './Dashboard';
 export { Route, RouteProps } from './Route';
