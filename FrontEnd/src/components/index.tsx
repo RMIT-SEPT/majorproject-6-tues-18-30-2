e5a482d4e0e26b693dedfb06f549005209819dac
Nick Mladenov <s3539747@student.rmit.edu.au>
2020-09-16 21:09:14 +1000

    Added the footer component to the root rollup for components.

--- FrontEnd/src/components/index.tsx
+++ FrontEnd/src/components/index.tsx
@@ -1,6 +1,7 @@
 export { App, AppProps } from './App';
 export { Header, HeaderProps } from './Header';
 export { MenuItem, MenuItemProps } from './MenuItem';
+export { Footer, FooterProps } from './Footer';
 export { Booking, BookingProps } from './Booking';
 export { Dashboard, DashboardProps } from './Dashboard';
 export { Route, RouteProps } from './Route';
