d1c48b406e06f3e97f9f143c3d8db860e0970e7c
Nick Mladenov <s3539747@student.rmit.edu.au>
2020-09-16 21:07:24 +1000

    Added the header component to the root rollup for components.

--- FrontEnd/src/components/index.tsx
+++ FrontEnd/src/components/index.tsx
@@ -1,4 +1,5 @@
 export { App, AppProps } from './App';
+export { Header, HeaderProps } from './Header';
 export { MenuItem, MenuItemProps } from './MenuItem';
 export { Booking, BookingProps } from './Booking';
 export { Dashboard, DashboardProps } from './Dashboard';
