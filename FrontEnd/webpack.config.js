c7595170dc9dd0dc2c1e1df17d2e86b52ecb9b5f
s3539747 <48439097+s3539747@users.noreply.github.com>
2020-09-19 21:21:48 +1000

    Frontend Production Builds (#29)
    * Removed the coverage report generated by the frontend build.
    
    * Removed unnecessary packages bloating the bundle(s) and added a ci optimised (production) build script.
    
    * Refactored the webpack configuration to detect environments and bundle differently depending on the vendors or main implementations as well as removed some unnecessary modules/plugins.
    
    * Modified the javascript build template to use the production build.
    
    * Updated the frontend directory case sensitivity.
    
    * Restored formilk as it's still in use.

--- FrontEnd/webpack.config.js
+++ FrontEnd/webpack.config.js
@@ -1,14 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
+module.exports = env => ({
+  mode: env === 'production' ? 'production' : 'development',
+  entry: {
+    main: './src/index.tsx',
+    vendor: [
+			'react',
+      'react-dom',
+      'react-router-dom',
+      'react-day-picker',
+      'antd',
+			'typestyle',
+      'history',
+      'formik'
+    ]
+  },
   resolve: {
     extensions: ['.ts', '.tsx', '.js']
   },
   output: {
+    filename: env === 'production' ? '[name].[hash].js' : 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.min.js',
+    library: 'vendor',
     publicPath: '/'
   },
   module: {
@@ -20,28 +34,6 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(scss)$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: function () {
-                return [require('precss'), require('autoprefixer')];
-              }
-            }
-          },
-          {
-            loader: 'sass-loader'
-          }
-        ]
       }
     ]
   },
@@ -53,4 +45,4 @@ module.exports = {
       template: './public/index.html'
     })
   ]
-};
+});
