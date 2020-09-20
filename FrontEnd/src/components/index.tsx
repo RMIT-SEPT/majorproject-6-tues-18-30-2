974e8eb1863b4b36ada4fd8a1fe481554a70fe0a
Nick Mladenov <s3539747@student.rmit.edu.au>
2020-09-21 00:20:20 +1000

    Added the menu modal to the global component roll up.

--- FrontEnd/src/components/index.tsx
+++ FrontEnd/src/components/index.tsx
@@ -1,6 +1,7 @@
 export { App, AppProps } from './App';
 export { Header, HeaderProps } from './Header';
 export { MenuItem, MenuItemProps } from './MenuItem';
+export { MenuModal, MenuModalProps } from './MenuModal';
 export { Footer, FooterProps } from './Footer';
 
 export { Booking, BookingProps } from './Booking';
