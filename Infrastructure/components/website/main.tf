3860480ba934445909a2775d09c8ca00f561bcd9
Nick Mladenov <s3539747@student.rmit.edu.au>
2020-09-20 14:23:47 +1000

    Added resources for creating an SSL certificate for the static website but left it commented out.

--- Infrastructure/components/website/main.tf
+++ Infrastructure/components/website/main.tf
@@ -7,6 +7,33 @@ provider "aws" {
   region  = var.region_name
 }
 
+#resource "aws_acm_certificate" "cert" {
+#  domain_name       = var.domain_name
+#  validation_method = "DNS"
+#
+#  lifecycle {
+#    create_before_destroy = true
+#  }
+#}
+#
+#resource "aws_route53_record" "validation" {
+#  zone_id = var.zone_id
+#  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
+#  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
+#  records = [
+#    aws_acm_certificate.cert.domain_validation_options.0.resource_record_value
+#  ]
+#  ttl     = 60
+#}
+#
+#resource "aws_acm_certificate_validation" "cert" {
+#  certificate_arn = aws_acm_certificate.cert.arn
+#
+#  validation_record_fqdns = [
+#    aws_route53_record.validation.fqdn
+#  ]
+#}
+
 resource "aws_route53_record" "a_record" {
   zone_id = var.zone_id
   name    = var.domain_name
