9b38012a0ecaa2b3fa5a25d9bcd284a1938a7357
Nick Mladenov <s3539747@student.rmit.edu.au>
2020-09-22 01:41:05 +1000

    Added the profile page to the global page rollup.

--- FrontEnd/src/pages/index.tsx
+++ FrontEnd/src/pages/index.tsx
@@ -4,3 +4,4 @@ export { Home } from './Home';
 export { Registration } from './Registration';
 export { Dashboard } from './Dashboard';
 export { BookingForm } from './BookingForm';
+export { Profile } from './Profile';
