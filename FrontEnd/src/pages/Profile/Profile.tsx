da04290b9dc3cf5cb5da350ce34f7768bef08720
Nick Mladenov <s3539747@student.rmit.edu.au>
2020-09-22 01:40:13 +1000

    Implemented a basic profile page.

---
+++ FrontEnd/src/pages/Profile/Profile.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FullWidthLayout } from '../../layouts';
+
+/**
+ * Profile Page
+ */
+export const Profile: React.FC = () => {
+  return (
+    <FullWidthLayout>
+      <h1>
+        This is the profile.
+      </h1>
+    </FullWidthLayout>
+  );
+};
