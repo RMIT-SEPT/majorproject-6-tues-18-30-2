7416f308c59e55a1116ee7aa9a21883a7904b115
s3601172 <30820975+s3601172@users.noreply.github.com>
2020-09-22 22:30:56 +1000

    code update (#50)
    * Deployment for login (#48)
    
    * Deployment files
    
    * Modified readme
    
    * dockerfile attempt (#44)
    
    * dockerfile attempt
    
    * fixed gitignore, restore + refactor files
    
    * Authentication Pipeline Containers (#49)
    
    * Integrated the container push for auth.
    
    * Added registry name variable reference.
    
    Co-authored-by: Muhammad Giorady <54261623+s3737757@users.noreply.github.com>
    Co-authored-by: s3539747 <48439097+s3539747@users.noreply.github.com>

---
+++ BackEnd/sept-dashboard/src/main/java/com/rmit/sept/dashboard/web/BookingController.java
@@ -0,0 +1,33 @@
+package com.rmit.sept.dashboard.web;
+
+import com.rmit.sept.dashboard.model.Booking;
+import com.rmit.sept.dashboard.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/dashboard")
+@CrossOrigin(origins = "http://localhost:8080")
+public class BookingController {
+    private static final Integer UPCOMING_STATUS = 1;
+    private static final Integer COMPLETE_STATUS = 2;
+
+    @Autowired
+    private BookingService bookingService;
+
+    @GetMapping("/upcoming-bookings")
+    @ResponseBody
+    public List<Booking> getUpcomingBookings(@RequestParam String customerId) {
+        return bookingService.findBookingByCustomerAndStatus(customerId, UPCOMING_STATUS);
+    }
+
+    @GetMapping("/completed-bookings")
+    @ResponseBody
+    public List<Booking> getCompletedBookings(@RequestParam String customerId) {
+        return bookingService.findBookingByCustomerAndStatus(customerId, COMPLETE_STATUS);
+    }
+
+
+}
