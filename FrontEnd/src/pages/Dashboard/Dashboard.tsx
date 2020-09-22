a06982f65fab92ac09fcc4470ded909be1e48db0
Thanh <s3601172@student.rmit.edu.au>
2020-09-22 15:45:11 +1000

    booking data as state

--- FrontEnd/src/pages/Dashboard/Dashboard.tsx
+++ FrontEnd/src/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { SideBarLayout } from '../../layouts';
 import { Row, Col, Table } from 'antd';
-import {getUpcomingBookings, getCompletedBookings} from '../../API/dashboard/DashboardService';
+import { getUpcomingBookings, getCompletedBookings } from '../../API/dashboard/DashboardService';
 
 /**
  * Dashboard Page
  */
 export const Dashboard: React.FC = () => {
+  const [upcomingBookings, setUpcomingBookings] = useState([]);
+  const [completedBookings, setCompletedBookings] = useState([]);
+
+  useEffect(() => {
+    setUpcomingBookings(getBookingData('u1@gmail.com', true));
+    setCompletedBookings(getBookingData('u1@gmail.com', false));
+  }, []);
+
   const columns = [
     {
       title: 'Booking Id',
@@ -46,11 +54,11 @@ export const Dashboard: React.FC = () => {
       <Row gutter={[24, 8]}>
         <Col order={1} span={12}>
           <h2>Upcoming Bookings</h2>
-          <Table dataSource={getBookingData('u1@gmail.com', true)} columns={columns} />
+          <Table dataSource={upcomingBookings} columns={columns} />
         </Col>
         <Col order={2} span={12}>
           <h2>Completed Bookings</h2>
-          <Table dataSource={getBookingData('u1@gmail.com', false)} columns={columns} />
+          <Table dataSource={completedBookings} columns={columns} />
         </Col>
       </Row>
     </SideBarLayout>
@@ -63,11 +71,9 @@ const getBookingData = (customerId: string, isUpcoming: boolean) => {
   if (isUpcoming == true) {
     getUpcomingBookings(customerId).then(response => {
       bookingData = response.data;
-      console.log(bookingData);
     });
   } else {
     getCompletedBookings(customerId).then(response => {
-      console.log(bookingData);
       bookingData = response.data;
     });
   }
