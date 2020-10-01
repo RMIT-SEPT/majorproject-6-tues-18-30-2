51f854d8aea15718e1717d127e975d9b04c810d9
Nick Mladenov <s3539747@student.rmit.edu.au>
2020-10-02 01:50:27 +1000

    Added the orchestration networking module for deployments.

--- Infrastructure/core/orchestration/main.tf
+++ Infrastructure/core/orchestration/main.tf
@@ -6,3 +6,13 @@ provider "aws" {
   version = ">= 2.28.1"
   region  = var.region_name
 }
+
+module "networking" {
+  source           = "./modules/networking"
+  environment_name = var.environment_name
+  resource_suffix  = var.resource_suffix
+  ip_block         = var.ip_block
+  ip_block_prefix  = var.ip_block_prefix
+  ip_block_offset  = var.ip_block_offset
+  eks_cluster_name = var.eks_cluster_name
+}
