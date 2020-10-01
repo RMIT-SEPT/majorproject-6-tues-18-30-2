f01a2a27631fd48dd3e9255f962d3ff7a95c7c12
Nick Mladenov <s3539747@student.rmit.edu.au>
2020-10-02 01:50:04 +1000

    Added the necessary environment variables for configuring the orchestration networking.

--- Infrastructure/core/orchestration/variables.tf
+++ Infrastructure/core/orchestration/variables.tf
@@ -15,3 +15,33 @@ variable "region_name" {
     error_message = "The AWS region name is not valid."
   }
 }
+
+variable "environment_name" {
+  description = "The environment name where the resources are being deployed into."
+  type        = string
+}
+
+variable "resource_suffix" {
+  description = "The resource suffix assigned to each AWS resource to distinguish the region and environment."
+  type        = string
+}
+
+variable "ip_block" {
+  description = "The reserved CIDR IP Address block used by the VPC."
+  type        = string
+}
+
+variable "ip_block_prefix" {
+  description = "The associated CIDR block prefix for calculating the amount of CIDR blocks in each subnetwork."
+  type        = number
+}
+
+variable "ip_block_offset" {
+  description = "The extension offset used to calculate public subnets in the CIDR block while avoiding private collisions."
+  type        = number
+}
+
+variable "eks_cluster_name" {
+  description = "The name of the EKS cluster."
+  type        = string
+}
