1976712b5e3e2911697bb81dd06108baabb826b6
Nick Mladenov <s3539747@student.rmit.edu.au>
2020-10-03 00:10:08 +1000

    Implemented the routing resources in asure via elasic load balancers with an SSL certificate being validated.

--- Infrastructure/core/orchestration/modules/routing/main.tf
+++ Infrastructure/core/orchestration/modules/routing/main.tf
@@ -1,3 +1,7 @@
+data "aws_route53_zone" "root_domain" {
+  name = var.root_domain
+}
+
 resource "aws_acm_certificate" "eks" {
   domain_name               = local.eks_domain
   subject_alternative_names = [ "*.${local.eks_domain}" ]
@@ -7,6 +11,19 @@ resource "aws_acm_certificate" "eks" {
     "name"        = local.eks_domain
     "environment" = var.environment_name
   }
+}
+
+resource "aws_route53_record" "eks_validation" {
+  name    = aws_acm_certificate.eks.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.eks.domain_validation_options.0.resource_record_type
+  zone_id = data.aws_route53_zone.root_domain.id
+  records = [ aws_acm_certificate.eks.domain_validation_options.0.resource_record_value ]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "eks_validation" {
+  certificate_arn         = aws_acm_certificate.eks.arn
+  validation_record_fqdns = [ aws_route53_record.eks_validation.fqdn ]
 }
 
 module "ingress" {
@@ -21,6 +38,24 @@ module "ingress" {
     "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-connection-idle-timeout" = "60",
     "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"                    = "elb"
   }
+}
+
+data "aws_elb_hosted_zone_id" "elb_zone_id" {}
+
+resource "aws_route53_record" "eks" {
+  zone_id = data.aws_route53_zone.root_domain.id
+  name    = local.eks_domain
+  type    = "A"
+
+  alias {
+    name                   = module.ingress.hostname
+    zone_id                = data.aws_elb_hosted_zone_id.elb_zone_id.id
+    evaluate_target_health = true
+  }
+
+  depends_on = [
+    module.ingress
+  ]
 }
 
 locals {
