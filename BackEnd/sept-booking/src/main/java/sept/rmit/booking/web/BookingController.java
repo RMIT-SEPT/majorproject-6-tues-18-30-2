63933edb0fbea9be677e62509d0ba94f73b523bb
Thanh Duong <s3601172@student.rmit.edu.au>
2020-10-02 15:19:33 +1000

    entity mapping and booking adding

---
+++ BackEnd/sept-booking/src/main/java/sept/rmit/booking/web/BookingController.java
@@ -0,0 +1,27 @@
+package sept.rmit.booking.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import sept.rmit.booking.model.Booking;
+import sept.rmit.booking.service.BookingService;
+
+@RestController
+@RequestMapping("/api/booking")
+@CrossOrigin(origins = "")
+public class BookingController {
+    @Autowired
+    private BookingService bookingService;
+
+    @PostMapping("add")
+    public @ResponseBody String addBooking(@RequestBody Booking booking){
+        boolean isExist = bookingService.isExist(booking.getCustomerId(), booking.getWorkingDateId());
+
+        if(isExist == false){
+            bookingService.create(booking.getCustomerId(), booking.getBookingDate(), booking.getWorkingDateId(), booking.getServiceId(), booking.getBookingStatusId());
+            return "New Booking Added";
+        }else{
+            return "Duplicate Entry";
+        }
+    }
+
+}
