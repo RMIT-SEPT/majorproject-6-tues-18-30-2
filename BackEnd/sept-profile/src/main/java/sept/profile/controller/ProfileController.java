d70812436252c997dc6c3f453ace7f766e1241f8
Muhammad Giorady <54261623+s3737757@users.noreply.github.com>
2020-10-03 15:51:58 +1000

    Code for update user profile attributes + fixed application.yaml in login as well (#63)

--- BackEnd/sept-profile/src/main/java/sept/profile/controller/ProfileController.java
+++ BackEnd/sept-profile/src/main/java/sept/profile/controller/ProfileController.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +30,28 @@ public class ProfileController {
 		Optional<User> user = userService.findByUsername(username);
 
 		return ResponseEntity.ok(user.get());
+	}
+	
+	@PutMapping("/profile")
+	public ResponseEntity<User> putProfile(HttpServletRequest request, @RequestBody User updatedUser) {
+		
+//		System.out.println(updatedUser.getDepartment());
+//		System.out.println(updatedUser.getCountry());
+//		System.out.println(updatedUser.getOrganisation());
+//		System.out.println(updatedUser.getFirstName());
+//		System.out.println(updatedUser.getLastName());
+		String username = (String) request.getAttribute("logged_user");
+
+		Optional<User> user = userService.findByUsername(username);
+		User aUser = user.get();
+		aUser.setFirstName(updatedUser.getFirstName());
+		aUser.setLastName(updatedUser.getLastName());
+		aUser.setDepartment(updatedUser.getDepartment());
+		aUser.setOrganisation(updatedUser.getOrganisation());
+		aUser.setCountry(updatedUser.getCountry());
+		
+		aUser = userService.updateProfile(aUser);
+		
+		return ResponseEntity.ok(aUser);
 	}
 }
