c220e57daa642416bfe2590f843feb10a7c69353
s3601172 <s3601172@student.rmit.edu.au>
2020-10-06 23:05:43 +1100

    Unit tests

--- BackEnd/sept-booking/src/main/java/sept/rmit/booking/web/BookingController.java
+++ BackEnd/sept-booking/src/main/java/sept/rmit/booking/web/BookingController.java
@@ -7,7 +7,7 @@ import sept.rmit.booking.service.BookingService;
 
 @RestController
 @RequestMapping("/api/booking")
-@CrossOrigin(origins = "")
+@CrossOrigin(origins = "localhost:8080")
 public class BookingController {
     @Autowired
     private BookingService bookingService;
